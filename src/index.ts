fed2a4c21829bd38e3dc88d68cf99b08185083b9
eina <109956262+encarbassotnopot@users.noreply.github.com>
2025-04-02 12:28:55 +0200

    primera versió. des de demanar a l'api d'spotify fins a un embed

--- src/index.ts
+++ src/index.ts
@@ -11,14 +11,30 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+import { CurrentlyPlaying } from 'spotify-types';
+import { getBearer } from './spotify-auth';
+
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
 		const url = new URL(request.url);
 		switch (url.pathname) {
-			case '/message':
-				return new Response('Hello, World!');
-			case '/random':
-				return new Response(crypto.randomUUID());
+			case '/spotid': {
+				const bearer = await getBearer();
+				const res = await fetch('https://api.spotify.com/v1/me/player/currently-playing', {
+					method: 'GET',
+					headers: {
+						Authorization: 'Bearer ' + bearer,
+					},
+				});
+				if (!res.ok || res.status != 200) {
+					console.log(`status: ${res.status} ${res.statusText}`);
+					console.log(await res.body);
+					return new Response('Spotify fetch error', { status: 500 });
+				}
+				const parsed = await res.json<CurrentlyPlaying>();
+				console.log(res);
+				return new Response(parsed.item?.uri);
+			}
 			default:
 				return new Response('Not Found', { status: 404 });
 		}
---
+++ src/spotify-auth.ts
@@ -0,0 +1,77 @@
+import { env } from 'cloudflare:workers';
+import { TokenResponse } from './interfaces';
+
+/**
+ * Gets either the current bearer token from the KV (if not expired) or gets a new one by calling {@link refreshToken}
+ * @returns A valid bearer token
+ */
+export async function getBearer(): Promise<string> {
+	const bearer = await env.KV.get('bearer');
+	if (bearer) return bearer;
+	else return await refreshToken();
+}
+
+/**
+ * Gets a new bearer token by refreshing.
+ * If no refresh token is found, calls {@link newAccessToken}
+ * The new access and refresh tokens are stored them in the KV.
+ * @throws Error if something goes wrong with the request.
+ * @returns A new access token
+ */
+async function refreshToken(): Promise<string> {
+	let refresh = await env.KV.get('refresh');
+	if (!refresh) return await newAccessToken();
+
+	const url = 'https://accounts.spotify.com/api/token';
+	const payload: RequestInit = {
+		method: 'POST',
+		headers: {
+			'content-type': 'application/x-www-form-urlencoded',
+			Authorization: 'Basic ' + Buffer.from(env.SPOTIFY_CLIENT_ID + ':' + env.SPOTIFY_CLIENT_SECRET).toString('base64'),
+		},
+		body: new URLSearchParams({
+			grant_type: 'refresh_token',
+			refresh_token: refresh,
+		}),
+	};
+	const response = await fetch(url, payload);
+	if (!response.ok) {
+		console.error(await response.json());
+		throw Error('Error fetching token with refresh');
+	}
+	const parsed = response.json<TokenResponse>();
+	env.KV.put('bearer', (await parsed).access_token, { expirationTtl: (await parsed).expires_in });
+	env.KV.put('refresh', (await parsed).refresh_token);
+	return (await parsed).access_token;
+}
+
+/**
+ * Gets a new bearer token by using the user-provided authorization code.
+ * The new access and refresh tokens are stored them in the KV.
+ * @throws Error if something goes wrong with the request.
+ * @returns The new access token
+ */
+async function newAccessToken(): Promise<string> {
+	const url = 'https://accounts.spotify.com/api/token';
+	const payload: RequestInit = {
+		method: 'POST',
+		headers: {
+			'content-type': 'application/x-www-form-urlencoded',
+			Authorization: 'Basic ' + Buffer.from(env.SPOTIFY_CLIENT_ID + ':' + env.SPOTIFY_CLIENT_SECRET).toString('base64'),
+		},
+		body: new URLSearchParams({
+			grant_type: 'authorization_code',
+			code: env.MY_AUTH_CODE,
+			redirect_uri: env.SPOTIFY_CLIENT_ENDPOINT,
+		}),
+	};
+	const response = await fetch(url, payload);
+	if (!response.ok) {
+		console.error(await response.json());
+		throw Error('Error fetching token with auth');
+	}
+	const parsed = response.json<TokenResponse>();
+	env.KV.put('bearer', (await parsed).access_token, { expirationTtl: (await parsed).expires_in });
+	env.KV.put('refresh', (await parsed).refresh_token);
+	return (await parsed).access_token;
+}
