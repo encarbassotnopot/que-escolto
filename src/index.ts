dfe03968774415a434b7b6c77f3d237df9026950
eina <109956262+encarbassotnopot@users.noreply.github.com>
2025-04-02 13:51:02 +0200

    request de currently_playing comuna a tots els endpoints. afegit coverart amb forward. veurem com va.

--- src/index.ts
+++ src/index.ts
@@ -11,29 +11,38 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
-import { CurrentlyPlaying } from 'spotify-types';
 import { getBearer } from './spotify-auth';
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
 		const url = new URL(request.url);
+		if (['/spotid', '/coverart'].every((el) => el === request.url)) return new Response('Not Found', { status: 404 });
+
+		const bearer = await getBearer();
+		const res = await fetch('https://api.spotify.com/v1/me/player/currently-playing', {
+			method: 'GET',
+			headers: {
+				Authorization: 'Bearer ' + bearer,
+			},
+		});
+		if (!res.ok || res.status != 200) {
+			console.log(`status: ${res.status} ${res.statusText}`);
+			console.log(await res.body);
+			return new Response('Spotify fetch error', { status: 500 });
+		}
+		const parsed = await res.json<SpotifyApi.CurrentlyPlayingResponse>();
+		console.log(res);
+
 		switch (url.pathname) {
-			case '/spotid': {
-				const bearer = await getBearer();
-				const res = await fetch('https://api.spotify.com/v1/me/player/currently-playing', {
-					method: 'GET',
-					headers: {
-						Authorization: 'Bearer ' + bearer,
-					},
-				});
-				if (!res.ok || res.status != 200) {
-					console.log(`status: ${res.status} ${res.statusText}`);
-					console.log(await res.body);
-					return new Response('Spotify fetch error', { status: 500 });
-				}
-				const parsed = await res.json<CurrentlyPlaying>();
-				console.log(res);
+			case '/spotid':
 				return new Response(parsed.item?.uri);
+			case '/coverart': {
+				switch (parsed.item?.type) {
+					case 'track':
+						return fetch(parsed.item?.album.images[0].url);
+					case 'episode':
+						return fetch(parsed.item?.images[0].url);
+				}
 			}
 			default:
 				return new Response('Not Found', { status: 404 });
